d293b04e9576e460afd8f0765eff60e16803ced8
HippoT <minhtuan.02071995@gmail.com>
2018-09-07 18:06:57 +0700

    Part 2

---
+++ src/ConfigurationApps/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConfigurationApps.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigurationApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UptimeService uptime;
+        public HomeController(UptimeService uptime)
+        {
+            this.uptime = uptime;
+        }
+        public IActionResult Index()
+        {
+            return View(new Dictionary<string, string> { ["message"] = "This is the Index action", ["Time"] = uptime.Uptime.ToString() });
+        }
+    }
+}
---
+++ src/ConfigurationApps/Infrastructure/ContentMiddleware.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationApps.Infrastructure
+{
+    public class ContentMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public ContentMiddleware(RequestDelegate next) => nextDelegate = next;
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if(httpContext.Request.Path.ToString().ToLower() == "/middleware")
+            {
+                await httpContext.Response.WriteAsync("This is from the content middleware", Encoding.UTF8);
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+    public class ShortCircuitMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public ShortCircuitMiddleware(RequestDelegate next) => nextDelegate = next;
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if(httpContext.Request.Headers["User-Agent"].Any(h => h.ToLower().Contains("edge")))
+            {
+                httpContext.Response.StatusCode = 403;
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+
+    public class BrowserTypeMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public BrowserTypeMiddleware(RequestDelegate next) => nextDelegate = next;
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Items["EdgeBrowser"] = httpContext.Request.Headers["User-Agent"].Any(v => v.ToLower().Contains("edge"));
+            await nextDelegate.Invoke(httpContext);
+        }
+    }
+
+    public class ErrorMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public ErrorMiddleware(RequestDelegate next)
+        {
+            nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await nextDelegate.Invoke(httpContext);
+            if (httpContext.Response.StatusCode == 403)
+            {
+                await httpContext.Response.WriteAsync("Edge not supported", Encoding.UTF8);
+            }
+            else if (httpContext.Response.StatusCode == 404)
+            {
+                await httpContext.Response.WriteAsync("No content middleware response", Encoding.UTF8);
+            }
+        }
+    }
+}
