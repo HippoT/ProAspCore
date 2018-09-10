87178b19d77211fdc111b0b26bcba5114879a954
HippoT <minhtuan.02071995@gmail.com>
2018-09-10 14:26:18 +0700

    config stuff

--- src/ConfigurationApps/Controllers/HomeController.cs
+++ src/ConfigurationApps/Controllers/HomeController.cs
@@ -4,18 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using ConfigurationApps.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace ConfigurationApps.Controllers
 {
     public class HomeController : Controller
     {
         private UptimeService uptime;
-        public HomeController(UptimeService uptime)
+        private ILogger<HomeController> logger;
+
+        public HomeController(UptimeService uptime, ILogger<HomeController> log)
         {
             this.uptime = uptime;
+            logger = log;
         }
         public IActionResult Index()
         {
+            logger.LogError($"Handled {Request.Path} at uptime {uptime.Uptime}");
             return View(new Dictionary<string, string> { ["message"] = "This is the Index action", ["Time"] = uptime.Uptime.ToString() });
         }
     }
--- src/ConfigurationApps/Startup.cs
+++ src/ConfigurationApps/Startup.cs
@@ -7,14 +7,31 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace ConfigurationApps
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<UptimeService>();
+            services.AddMvc().AddMvcOptions(options =>
+            {
+                options.RespectBrowserAcceptHeader = true;
+            });
+
+            services.AddMvc();
+        }
+
+        public void ConfigureDevelopmentServices(IServiceCollection services)
         {
             services.AddSingleton<UptimeService>();
             services.AddMvc();
@@ -23,20 +40,40 @@ namespace ConfigurationApps
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            //var a = Configuration.GetSection("ShortCircuitMiddleware");
+            //var b = a.GetValue<bool>("EnableBrowserShortCircuit");
+            //var c = b.Value;
+
+            if ((Configuration.GetSection("ShortCircuitMiddleware")?.GetValue<bool>("EnableBrowserShortCircuit")).Value)
+            {
+                app.UseMiddleware<ErrorMiddleware>();
+                app.UseMiddleware<ShortCircuitMiddleware>();
+                app.UseMiddleware<ContentMiddleware>();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMiddleware<ErrorMiddleware>();
-            app.UseMiddleware<ShortCircuitMiddleware>();
-            app.UseMiddleware<ContentMiddleware>();
+            app.UseStaticFiles();
+            
             
             app.UseMvcWithDefaultRoute();
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+        }
+
+        public void ConfigureDevelopment(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseBrowserLink();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
