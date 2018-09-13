308ef4a8f3e1d72c3c0282d59d41cf7d8f97fdb1
HippoT <minhtuan.02071995@gmail.com>
2018-09-13 16:47:58 +0700

    Add project controller and action

---
+++ src/Controllers/Controllers/DerivedController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public IActionResult Index()
+        {
+            string page = "index";
+            return View(page, $"This is a derived controller");
+        }
+
+        public ViewResult Headers() => View("DictionaryResult", Request.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.FirstOrDefault()));
+    }
+}
---
+++ src/Controllers/Controllers/PocoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Controllers.Controllers
+{
+    public class PocoController : Controller
+    {
+        public ViewResult Index()
+        {
+            return new ViewResult()
+            {
+                ViewName = "index",
+                ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary())
+                {
+                    Model = $"This is a POCO controller"
+                }
+            };
+        }
+    }
+}
