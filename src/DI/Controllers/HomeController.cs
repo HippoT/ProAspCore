8a5e29c574220a22f8a7eb4a4cc5d43c3f164c50
HippoT <minhtuan.02071995@gmail.com>
2018-09-13 18:12:07 +0700

    DI

---
+++ src/DI/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DI.Models;
+
+namespace DI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View(new MemoryRepository().Products);
+    }
+}
