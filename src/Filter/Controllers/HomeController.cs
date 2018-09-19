924edae0f7479a5536b816e72f23fe378b59e887
HippoT <minhtuan.02071995@gmail.com>
2018-09-19 11:33:05 +0700

    Filter

---
+++ src/Filter/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Filter.Infrastructure;
+
+namespace Filter.Controllers
+{
+    [ViewResultDetails]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index", "This is the index action on the Home Controller");
+        }
+
+        public ViewResult SecondAction() => View("Index", "This is the second action on the Home controller");
+    }
+}
